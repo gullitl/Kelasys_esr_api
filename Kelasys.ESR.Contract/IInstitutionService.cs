d22968d830e70cc0adf8e88e5ef8e11ed2148291
plusembo <plam.l@live.fr>
2020-04-27 01:01:30 -0400

    Rectification du projet

--- Kelasys.ESR.Contract/IInstitutionService.cs
+++
@@ -1,6 +0,0 @@
-﻿using Kelasys.esr.Models.Entities;
-
-namespace Kelasys.esr.Contracts {
-    public interface IInstitutionService : ICrud<Institution> {
-    }
-}
--- Kelasys.ESR.Utils/Enumerations/NiveauxEnseignement.cs
+++
@@ -1,9 +0,0 @@
-﻿using System.ComponentModel;
-
-namespace Kelasys.esr.Utils.Enumerations {
-    public enum NiveauxEnseignement {
-        [Description("Maternel")] Maternel,
-        [Description("Primaire")] Primaire,
-        [Description("Secondaire")] Secondaire
-    }
-}
